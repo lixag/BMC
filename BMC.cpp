75c20bb612a600fafb53283c6d715af51160d48b
xlapt <viclx1223@gmail.com>
2019-12-07 01:49:56 -0500

    refactor version 1

--- BMC.cpp
+++ BMC.cpp
@@ -50,65 +50,46 @@ private:
   }
 
 public:
-  FunctionDeclVisitor(ASTContext *ctx) : Ctx{ctx} {}
+  explicit FunctionDeclVisitor(ASTContext *ctx) : Ctx{ctx} {}
 
+  static std::string binaryOp(const Stmt *stmt,
+                              std::unordered_map<std::string, int> &SSATable) {
+    auto *B = cast<BinaryOperator>(stmt);
+    auto *RHS = B->getRHS();
+    std::string R;
+    if (auto *Implicit = dyn_cast<ImplicitCastExpr>(RHS))
+      RHS = Implicit->getSubExpr();
+    if (auto *BOP = dyn_cast<BinaryOperator>(RHS)) {
+      R = binaryOp(BOP, SSATable);
+    } else if (auto *IntLit = dyn_cast<IntegerLiteral>(RHS)) {
+      R = IntLit->getValue().toString(10, false);
+    } else if (auto *DRE = dyn_cast<DeclRefExpr>(RHS)) {
+      if (auto *VD = dyn_cast<VarDecl>(DRE->getDecl())) {
+        std::string Var = VD->getQualifiedNameAsString();
+        R = Var + std::to_string(SSATable[Var]);
+      }
+    }
+
+    auto *LHS = B->getLHS();
+    std::string L;
+    if (auto *Implicit = dyn_cast<ImplicitCastExpr>(LHS))
+      LHS = Implicit->getSubExpr();
+    if (auto *DRE = dyn_cast<DeclRefExpr>(LHS)) {
+      if (auto *VD = dyn_cast<VarDecl>(DRE->getDecl())) {
+        std::string Var = VD->getQualifiedNameAsString();
+        L = B->isAssignmentOp()
+                ? Var + std::to_string(++SSATable[Var]) + "=="
+                : Var + std::to_string(SSATable[Var]) + B->getOpcodeStr().str();
+      }
+    }
+    return L + R;
+  }
   // generating ssa form formula here
   void processStmt(std::vector<std::string> &Clause, const Stmt *stmt,
                    std::unordered_map<std::string, int> &SSATable) {
     switch (stmt->getStmtClass()) {
     case Stmt::BinaryOperatorClass: {
-      std::string formula;
-      auto *B = cast<BinaryOperator>(stmt);
-      auto *LHS = B->getLHS();
-      auto *RHS = B->getRHS();
-
-      // Normally it's assignment and comp here
-      if (auto *Implicit = dyn_cast<ImplicitCastExpr>(LHS))
-        LHS = Implicit->getSubExpr();
-      if (auto *DRE = dyn_cast<DeclRefExpr>(LHS)) {
-        if (auto *VD = dyn_cast<VarDecl>(DRE->getDecl())) {
-          std::string Var = VD->getQualifiedNameAsString();
-          if (B->isAssignmentOp()) {
-            formula.append(Var + std::to_string(++SSATable[Var]));
-            formula.append("==");
-          } else {
-            formula.append(Var + std::to_string(SSATable[Var]));
-            formula.append(B->getOpcodeStr());
-          }
-        }
-      }
-      // else if (auto *UOP = dyn_cast<UnaryOperator>(LHS)) {
-      //}
-
-      if (auto *IntLit = dyn_cast<IntegerLiteral>(RHS)) {
-        formula.append(IntLit->getValue().toString(10, false));
-      } else if (auto *BOP = dyn_cast<BinaryOperator>(RHS)) {
-        // should be add, sub, mul, div here
-        auto *LHS1 = BOP->getLHS();
-        auto *RHS1 = BOP->getRHS();
-        if (auto *Implicit = dyn_cast<ImplicitCastExpr>(LHS1))
-          LHS1 = Implicit->getSubExpr();
-        if (auto *Implicit = dyn_cast<ImplicitCastExpr>(RHS1))
-          RHS1 = Implicit->getSubExpr();
-        if (auto *DRE = dyn_cast<DeclRefExpr>(LHS1)) {
-          if (auto *VD = dyn_cast<VarDecl>(DRE->getDecl())) {
-            std::string Var = VD->getQualifiedNameAsString();
-            formula.append(Var + std::to_string(SSATable[Var]));
-            formula.append(BOP->getOpcodeStr());
-          }
-        }
-        if (auto *IntLit1 = dyn_cast<IntegerLiteral>(RHS1)) {
-          formula.append(IntLit1->getValue().toString(10, false));
-        } else {
-          if (auto *DRE = dyn_cast<DeclRefExpr>(RHS1)) {
-            if (auto *VD = dyn_cast<VarDecl>(DRE->getDecl())) {
-              std::string Var = VD->getQualifiedNameAsString();
-              formula.append(Var + std::to_string(SSATable[Var]));
-            }
-          }
-        }
-      }
-      Clause.push_back(formula);
+      Clause.push_back(binaryOp(stmt, SSATable));
       break;
     }
     case Stmt::DeclStmtClass: {
@@ -127,7 +108,7 @@ public:
             SSATable[Var];
           }
         }
-        auto Loc = Ctx->getFullLoc(stmt->getBeginLoc());
+        // only var decl
         if (!formula.empty())
           Clause.push_back(formula);
       }
@@ -145,103 +126,7 @@ public:
     }
   }
 
-  bool VisitFunctionDecl(FunctionDecl *Decl) {
-    if (!Ctx->getSourceManager().isWrittenInMainFile(Decl->getBeginLoc()))
-      return true;
-    FullSourceLoc full_location = Ctx->getFullLoc(Decl->getBeginLoc());
-    std::vector<Formula> Fs;
-    if (full_location.isValid()) {
-      if (Decl->hasBody()) {
-        auto *FuncBody = Decl->getBody();
-        std::unique_ptr<CFG> FuncCFG =
-            CFG::buildCFG(Decl, FuncBody, Ctx, CFG::BuildOptions());
-        // FuncCFG->dump(LangOptions(), false);
-        // FuncCFG->viewCFG(LangOptions());
-
-        BlockPath Path;
-        dfsHelper(Paths, Path, &FuncCFG->getEntry());
-        errs() << "size: " << Paths.size() << "\n";
-        for (auto &P : Paths) {
-          std::vector<std::string> Clauses;
-          std::unordered_map<std::string, int> SSATable;
-          errs() << "path: ";
-          for (auto &N : P) {
-            errs() << N->getBlockID() << ' ' << N << ' ';
-            for (auto &E : *N) {
-              auto *S = E.castAs<CFGStmt>().getStmt();
-              if (!(S->getStmtClass() == Stmt::BinaryOperatorClass) and
-                  !(S->getStmtClass() == Stmt::UnaryOperatorClass) and
-                  !(S->getStmtClass() == Stmt::DeclStmtClass))
-                continue;
-              processStmt(Clauses, S, SSATable);
-            }
-            if (N->succ_empty())
-              continue;
-            if (N == P.back())
-              continue;
-            CFGBlock *NextBlock = *std::next(&N);
-            // errs() << "NextBlock Addr: " << NextBlock << '\n';
-            auto ID = NextBlock->getBlockID();
-            // look at the terminator
-            auto T = N->getTerminator();
-            auto *St = T.getStmt();
-            if (!St)
-              continue;
-            if (isa<IfStmt>(St)) {
-              // rewrite the last the stmt in N
-              if ((*N->succ_begin())->getBlockID() != ID)
-                Clauses.back() = "!(" + Clauses.back() + ")";
-            } else if (auto *SwitchSt = dyn_cast<SwitchStmt>(St)) {
-              auto *LB = NextBlock->getLabel();
-              if (LB == nullptr)
-                continue;
-              const auto Last = Clauses.back();
-              Clauses.pop_back();
-              if (isa<DefaultStmt>(LB)) {
-                for (auto *FirstCase = SwitchSt->getSwitchCaseList();
-                     FirstCase->getNextSwitchCase() != nullptr;
-                     FirstCase = FirstCase->getNextSwitchCase()) {
-                  if (auto *CaseSt = dyn_cast<CaseStmt>(FirstCase)) {
-                    if (!CaseSt->getLHS())
-                      continue;
-                    if (auto *ConstSt =
-                            dyn_cast<ConstantExpr>(CaseSt->getLHS())) {
-                      auto *CaseVal = ConstSt->getSubExpr();
-                      if (auto *IntLit = dyn_cast<IntegerLiteral>(CaseVal)) {
-                        auto Val = IntLit->getValue().toString(10, false);
-                        Clauses.push_back('(' + Last + ")!=" + Val);
-                      }
-                    }
-                  }
-                }
-              } else if (auto *CaseSt = dyn_cast<CaseStmt>(LB)) {
-                if (!CaseSt->getLHS())
-                  continue;
-                if (auto *ConstSt = dyn_cast<ConstantExpr>(CaseSt->getLHS())) {
-                  auto Val = ConstSt->getResultAsAPSInt().toString(10, false);
-                  Clauses.push_back('(' + Last + ")==" + Val);
-                }
-              }
-            }
-          }
-          errs() << '\n';
-
-          Fs.push_back({Clauses, SSATable});
-        }
-      }
-    }
-    //    int cnt = 1;
-    //    for (auto &F : Fs) {
-    //      errs() << "path" << cnt << ": size " << F.Clauses.size() << ' ';
-    //      for (auto &Elem : F.Clauses)
-    //        errs() << Elem << ' ';
-    //      errs() << "\nSSATable" << cnt << ": ";
-    //      for (auto &Elem : F.SSATable)
-    //        errs() << Elem.first << ' ' << Elem.second << ' ';
-    //      errs() << '\n';
-    //      ++cnt;
-    //    }
-
+  static void generateZ3Code(std::vector<Formula> &Fs) {
     int fileNum = 0;
     for (auto &F : Fs) {
       std::error_code EC;
@@ -250,38 +135,118 @@ public:
             "#include <iostream>\n"
             "using namespace z3;\n"
             "int main() {\n"
-            "  context c;\n"
-            "  solver s(c);\n";
+            "    context c;\n"
+            "    solver s(c);\n";
 
       for (auto &C : F.SSATable) {
         for (int i = 1; i <= C.second; ++i)
-          OS << "  expr " << C.first << i << " = c.int_const(\"" << C.first << i
-             << "\");\n";
+          OS << "    expr " << C.first << i << " = c.int_const(\"" << C.first
+             << i << "\");\n";
       }
 
       int num = 1;
       for (auto &Clause : F.Clauses) {
-        OS << "  expr conjecture" << num << " = " << Clause << ";\n";
-        OS << "  s.add(conjecture" << num << ");\n";
+        OS << "    expr conjecture" << num << " = " << Clause << ";\n";
+        OS << "    s.add(conjecture" << num << ");\n";
         ++num;
       }
 
       OS << "  s.check();\n";
-      OS << R"(  switch (s.check()) {
-                     case unsat:
-                         std::cout << "this path is valid";
-                         break;
-                     case sat:
-                         std::cout <<"this path is not valid";
-                         break;
-                     case unknown:
-                         std::cout <<"unknown";
-                         break;
-                 })";
+      OS << R"(    switch (s.check()) {
+                       case unsat:
+                           std::cout << "this path is valid";
+                           break;
+                       case sat:
+                           std::cout <<"this path is not valid";
+                           break;
+                       case unknown:
+                           std::cout <<"unknown";
+                           break;
+                   })";
       OS << "\n}\n";
       ++fileNum;
     }
+  }
 
+  static Optional<std::string> caseStmtVal(CaseStmt *CaseSt) {
+    if (!CaseSt->getLHS())
+      return None;
+    if (auto *ConstSt = dyn_cast<ConstantExpr>(CaseSt->getLHS())) {
+      auto *CaseVal = ConstSt->getSubExpr();
+      if (auto *IntLit = dyn_cast<IntegerLiteral>(CaseVal))
+        return IntLit->getValue().toString(10, false);
+    }
+    return None;
+  }
+
+  static void processBranch(CFGBlock *N, std::vector<std::string> &Clauses,
+                            std::unordered_map<std::string, int> &SSATable) {
+    if (N->succ_empty())
+      return;
+    auto *NextBlock = *std::next(&N);
+    auto ID = NextBlock->getBlockID();
+    auto T = N->getTerminator();
+    auto *St = T.getStmt();
+    if (!St)
+      return;
+    if (isa<IfStmt>(St)) {
+      // rewrite the last the stmt in N
+      if ((*N->succ_begin())->getBlockID() != ID)
+        Clauses.back() = "!(" + Clauses.back() + ")";
+    } else if (auto *SwitchSt = dyn_cast<SwitchStmt>(St)) {
+      auto *LB = NextBlock->getLabel();
+      if (LB == nullptr)
+        return;
+      const auto Last = Clauses.back();
+      Clauses.pop_back();
+      if (isa<DefaultStmt>(LB)) {
+        for (auto *FirstCase = SwitchSt->getSwitchCaseList(); FirstCase;
+             FirstCase = FirstCase->getNextSwitchCase()) {
+          if (auto *CaseSt = dyn_cast<CaseStmt>(FirstCase))
+            if (auto Val = caseStmtVal(CaseSt))
+              Clauses.push_back('(' + Last + ")!=" + Val.getValue());
+        }
+      } else if (auto *CaseSt = dyn_cast<CaseStmt>(LB)) {
+        if (auto Val = caseStmtVal(CaseSt))
+          Clauses.push_back('(' + Last + ")==" + Val.getValue());
+      }
+    }
+  }
+
+  bool VisitFunctionDecl(FunctionDecl *Decl) {
+    if (!Ctx->getSourceManager().isWrittenInMainFile(Decl->getBeginLoc()))
+      return true;
+    FullSourceLoc full_location = Ctx->getFullLoc(Decl->getBeginLoc());
+    std::vector<Formula> Fs;
+    if (full_location.isValid()) {
+      if (Decl->hasBody()) {
+        auto *FuncBody = Decl->getBody();
+        auto FuncCFG = CFG::buildCFG(Decl, FuncBody, Ctx, CFG::BuildOptions());
+        // FuncCFG->dump(LangOptions(), false);
+        // FuncCFG->viewCFG(LangOptions());
+
+        BlockPath Path;
+        dfsHelper(Paths, Path, &FuncCFG->getEntry());
+
+        for (auto &P : Paths) {
+          std::vector<std::string> Clauses;
+          std::unordered_map<std::string, int> SSATable;
+          for (auto &N : P) {
+            for (auto &E : *N) {
+              auto *S = E.castAs<CFGStmt>().getStmt();
+              if (!(S->getStmtClass() == Stmt::BinaryOperatorClass) &&
+                  !(S->getStmtClass() == Stmt::UnaryOperatorClass) &&
+                  !(S->getStmtClass() == Stmt::DeclStmtClass))
+                continue;
+              processStmt(Clauses, S, SSATable);
+            }
+            processBranch(N, Clauses, SSATable);
+            Fs.push_back({Clauses, SSATable});
+          }
+        }
+      }
+      generateZ3Code(Fs);
+    }
     return true;
   }
 }; // namespace
